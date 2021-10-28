007aa607e78815bd4169c9a465e3622d80a72156
Jacob Overgaard <overgaard@gmail.com>
2021-10-28 22:40:29 +0200

    Add reddit fetcher

---
+++ lib/reddit.ts
@@ -0,0 +1,32 @@
+import { pick } from "lodash";
+
+export async function tryGetSubreddit(name: string) {
+  const baseUrl = "https://www.reddit.com/";
+  if (!name) {
+    throw new Error("No name provided");
+  }
+
+  const meta = await fetch(`${baseUrl}r/${name}.json`)
+    .then((r) => r.json())
+    .catch(() => undefined);
+
+  if (!meta) {
+    throw new Error("No data");
+  }
+
+  let posts = meta.data.children
+    .filter((post) => !post.data.stickied)
+    .map((post) => post.data);
+  posts = posts.map((post) =>
+    pick(post, [
+      "id",
+      "title",
+      "selftext",
+      "selftext_html",
+      "author_fullname",
+      "created_utc",
+      "url",
+    ])
+  );
+  return posts;
+}

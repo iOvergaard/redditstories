e85c5ab8367c706f21778b5cd644489ba5407e5c
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2021-10-31 14:11:26 +0100

    Add more links

--- lib/links.ts
+++ lib/links.ts
@@ -1,6 +1,17 @@
 const links = [
     "/r/talesfromtechsupport",
     "/r/talesfromretail",
+    "/r/perfecttiming",
+    "/r/100yearsago",
+    "/r/nostalgia",
+    "/r/tifu",
+    "/r/justnomil",
+    "/r/askwomen",
+    "/r/askmen",
+    "/r/askhistorians",
+    "/r/bestof",
+    "/r/explainitlikeimfive",
+    "/r/outoftheloop",
     "/r/gonewildstories"
 ];
 

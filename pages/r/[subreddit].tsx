48b5b133b1c19c0ac56d64269db436f38db830c3
jov <752371+iOvergaard@users.noreply.github.com>
2022-02-17 21:53:08 +0100

    Append new posts to bottom of page instead of replacing

--- pages/r/[subreddit].tsx
+++ pages/r/[subreddit].tsx
@@ -1,16 +1,13 @@
-import {
-  GetStaticPathsResult,
-  GetStaticPropsContext,
-  GetStaticPropsResult,
-  NextPage,
-} from "next";
-import Head from "next/head";
-import { useEffect, useState } from "react";
-import Layout from "../../components/layout";
-import Post from "../../components/post";
-import { tryGetSubreddit } from "../../lib/reddit";
-import styles from "../../styles/Subreddit.module.css";
-import paths from "../../lib/links";
+import { GetStaticPathsResult, GetStaticPropsContext, GetStaticPropsResult, NextPage } from 'next';
+import Head from 'next/head';
+import { useState } from 'react';
+
+import Layout from '../../components/layout';
+import Post from '../../components/post';
+import paths from '../../lib/links';
+import { tryGetSubreddit } from '../../lib/reddit';
+import styles from '../../styles/Subreddit.module.css';
+
 
 type Props = {
   subredditName: string;
@@ -23,9 +20,9 @@ const Subreddit: NextPage<Props> = (props: Props) => {
   const [posts, setPosts] = useState<any>(props.subreddit.posts);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, [posts]);
+  // useEffect(() => {
+  //   window.scrollTo({ top: 0, behavior: "smooth" });
+  // }, [posts]);
 
   const onLoadMore = async () => {
     setIsLoading(true);
@@ -34,9 +31,10 @@ const Subreddit: NextPage<Props> = (props: Props) => {
 
     if (response) {
       setAfter(response.after);
-      setCount(count + 25);
-      setPosts(response.posts);
+      setCount(oldCount => oldCount + 25);
+      setPosts((oldPosts: any) => [...oldPosts, ...response.posts]);
     }
+
     setIsLoading(false);
   };
 
@@ -53,10 +51,10 @@ const Subreddit: NextPage<Props> = (props: Props) => {
 
       {posts?.length
         ? posts.map((post: any) => (
-            <div key={post.id} className={styles.post}>
-              <Post post={post} />
-            </div>
-          ))
+          <div key={post.id} className={styles.post}>
+            <Post post={post} />
+          </div>
+        ))
         : "No posts found"}
       <button type="button" onClick={onLoadMore}>
         {isLoading ? "Loading..." : "Load more"}

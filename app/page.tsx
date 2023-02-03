6a5dcddf34046c00feb6701f43543f062d0cf9d9
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-02-03 16:05:27 +0100

    force frontpage to be static

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import styles from "./Home.module.css";
 import links from "../lib/links";
 import Search from "./Search";
 
+export const dynamic = "force-static";
+export const revalidate = false;
+
 export default function Home() {
   return (
     <>

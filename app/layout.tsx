fa35437e677933b48aaeac7a82c5047a71754d37
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-05-12 21:27:23 +0200

    add default title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import "./global.css";
 import React from "react";
 import styles from "./Layout.module.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | RedditStories',
+    default: 'RedditStories',
+  },
+};
 
 export default function RootLayout({
   children,

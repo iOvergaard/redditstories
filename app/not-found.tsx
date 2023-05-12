68bd03d6f494be2725002697e4945f55153f13b1
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-05-12 21:58:10 +0200

    not found boundary

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <>
+      <Link href="/" passHref>
+        <h1>Not Found</h1>
+      </Link>
+      <p>Could not find requested resource</p>
+    </>
+  );
+}

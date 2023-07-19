5ddc8b3f474e32ebd7138768d555c11bde082e8b
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-07-19 16:37:53 +0200

    add support for slugs and use them to render the next page

---
+++ app/r/[...slugs]/page.tsx
@@ -0,0 +1,72 @@
+import { Metadata } from "next"
+import Link from "next/link"
+import { notFound } from "next/navigation"
+import styles from "./Subreddit.module.css"
+import Post from "./post"
+import { tryGetSubreddit } from "@/lib/reddit"
+
+type Props = {
+  params: {
+    slugs: string[]
+  }
+}
+
+export const revalidate = 10
+
+export function generateMetadata({ params: { slugs } }: Props): Metadata {
+  const subreddit = slugs[0]
+
+  return {
+    title: `r/${subreddit}`,
+    description: `r/${subreddit} on reddit`,
+  }
+}
+
+export default async function Page({ params: { slugs } }: Props) {
+  console.log("slugs", slugs)
+  const subreddit = slugs[0]
+  const after = slugs[1]
+  const count = slugs[2] ? parseInt(slugs[2]) : 0
+
+  const data = await tryGetSubreddit(subreddit, { after, count })
+
+  if (!data) {
+    notFound()
+  }
+
+  const renderNextPageLink = () => {
+    if (!data.after) {
+      return null
+    }
+
+    const nextCount = count + 25
+
+    return (
+      <Link href={`/r/${subreddit}/${data.after}/${nextCount}`}>
+        Next page →
+      </Link>
+    )
+  }
+
+  const renderRefreshLink = () => (
+    <Link href={`/r/${subreddit}`}>Refresh 🔃</Link>
+  )
+
+  return (
+    <article>
+      <Link href="/" passHref>
+        <h1 className={styles.title}>{subreddit}</h1>
+      </Link>
+      {data.posts?.length
+        ? data.posts.map((post: any, index: number) => (
+            <Post key={post.id} post={post} isFirst={index === 0} />
+          ))
+        : "This subreddit has no posts"}
+      <footer>
+        {renderRefreshLink()}
+        &nbsp;|&nbsp;
+        {renderNextPageLink()}
+      </footer>
+    </article>
+  )
+}
